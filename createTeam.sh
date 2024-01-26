ba06ae165d8b7e14b8cc35c5bbf54f93d92056d1
peddappagari upendra reddy <97207144+Upendraredd@users.noreply.github.com>
2024-01-26 14:36:02 +0530

    Delete createTeam.sh

--- createTeam.sh
+++
@@ -1,8 +0,0 @@
-echo "Create a team"
-echo "Please enter the PAT"
-read pat
-curl \
-  -X POST \
- -u upendraredd:$pat \
-  https://github.com/Upendraredd/gitrepo \
-  -d '{"name":"facebook-devopsteam","description":"A great team","permission":"push","privacy":"closed"}'
